8b5bd3254c2d6d58de8900e9ec850fff727edbf7
Anastasiia Bondarenko <bondarenkonastya02@icloud.com>
2024-06-07 13:36:21 +0200

    Add kgs

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,11 +8,17 @@ public class Main{
         System.out.println("Converter App");
 
         double kgs = 5;
+        double pnds = 12;
         double pounds = convKgsToPounds(kgs);
-        System.out.println("Pounds is " + pounds + " pounds.");
+        double kilos = convPoundsToKgs(pnds);
+        System.out.println("Pounds is " + pounds + " pounds and " + kilos + " kgs.");
     }
 
     private static double convKgsToPounds(double kgs){
         return kgs * CONV_K;
+    }
+
+    private static double convPoundsToKgs(double pnds){
+        return pnds / CONV_K;
     }
 }
