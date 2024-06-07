216f04144345174f0f7b924665ee9f1cc002248c
Anastasiia Bondarenko <bondarenkonastya02@icloud.com>
2024-06-07 13:44:29 +0200

    Add description

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,7 @@ public class Main{
 
     public static void main(String[] args){
         System.out.println("Converter App");
+        System.out.println("App for measures converting.");
 
         double kgs = 5;
         double pounds = convKgsToPounds(kgs);
