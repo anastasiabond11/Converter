5ad00b0f3d765339b4b21422a512a9ab00df687e
Anastasiia Bondarenko <bondarenkonastya02@icloud.com>
2024-06-07 13:49:21 +0200

    Add app version

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,7 @@ public class Main{
 
     public static void main(String[] args){
         System.out.println("Converter App");
+        System.out.println("Version 1.0.");
 
         double kgs = 5;
         double pnds = 12;
